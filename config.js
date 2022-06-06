d66846be744a920c8603f9d1fc53cd1e020afa94
hatranthuylan <106941674+hatranthuylan@users.noreply.github.com>
2022-06-06 10:40:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Thứ 5,có bất ngờ cho bạn.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100077496657718
+    messLink: 'https://www.facebook.com/messages/t/100077496657718'
 }
